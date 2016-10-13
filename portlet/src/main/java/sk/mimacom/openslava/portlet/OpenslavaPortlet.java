fc6ff8ad1924bbd50623c4d24e99c6fb5bfa7d19
xco-sk <marek@xco.sk>
2016-10-13 13:16:04 +0200

    Reference whois service from portlet and resolve whois data from users input

--- portlet/src/main/java/sk/mimacom/openslava/portlet/OpenslavaPortlet.java
+++ portlet/src/main/java/sk/mimacom/openslava/portlet/OpenslavaPortlet.java
@@ -1,20 +1,54 @@
 package sk.mimacom.openslava.portlet;
 
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+import com.liferay.portal.kernel.util.ParamUtil;
+
+import demo.whois.api.SiteInfoDTO;
+import demo.whois.api.WhoisService;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
 import javax.portlet.Portlet;
+import javax.portlet.ProcessAction;
+
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
 
 @Component(
-	immediate = true,
-	property = {
-		"com.liferay.portlet.display-category=Openslava",
-		"com.liferay.portlet.instanceable=false",
-		"javax.portlet.display-name=Openslava portlet",
-		"javax.portlet.init-param.template-path=/jsp/",
-		"javax.portlet.init-param.view-template=/jsp/view.jsp"
-	},
-	service = Portlet.class
+    immediate = true,
+    property = {
+        "com.liferay.portlet.display-category=Openslava",
+        "com.liferay.portlet.header-portlet-css=/css/main.css",
+        "com.liferay.portlet.instanceable=false",
+        "javax.portlet.display-name=Openslava portlet",
+        "javax.portlet.init-param.template-path=/jsp/",
+        "javax.portlet.init-param.view-template=/jsp/view.jsp",
+        "javax.portlet.resource-bundle=content.Language"
+    },
+    service = Portlet.class
 )
 public class OpenslavaPortlet extends MVCPortlet {
 
+  @Reference
+  WhoisService service;
+  
+  @ProcessAction(name = "whoisLookup")
+  public void getWhoisData(ActionRequest request, ActionResponse response){
+    
+    String url = ParamUtil.getString(request, "url");
+    
+    if(url != null){
+      try {
+        SiteInfoDTO siteInfo = service.getSiteInfo(new URI(null,url,null,null));
+        request.setAttribute("siteInfo", siteInfo);
+      } catch (URISyntaxException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+  
 }
--- portlet/src/main/resources/META-INF/resources/jsp/view.jsp
+++ portlet/src/main/resources/META-INF/resources/jsp/view.jsp
@@ -1,2 +1,41 @@
 <%@ include file="init.jsp" %>
+
 <h1><liferay-ui:message key="whois.title" /></h1>
+
+<c:if test="${not empty siteInfo}">
+<% SiteInfoDTO info = (SiteInfoDTO) request.getAttribute("siteInfo"); %>
+<h3><liferay-ui:message key="whois.owner"/></h3>
+<dl class="dl-horizontal">
+  <dt><liferay-ui:message key="whois.person"/></dt>
+  <dd><%= info.owner.person %></dd>
+  
+  <dt><liferay-ui:message key="whois.organization"/></dt>
+  <dd><%= info.owner.organization %></dd>
+  
+  <dt><liferay-ui:message key="whois.phone"/></dt>
+  <dd><%= info.owner.phoneNumber %></dd>
+</dl>
+<h3><liferay-ui:message key="whois.admin"/></h3>
+<dl class="dl-horizontal">
+  <dt><liferay-ui:message key="whois.person"/></dt>
+  <dd><%= info.admin.person %></dd>
+  
+  <dt><liferay-ui:message key="whois.organization"/></dt>
+  <dd><%= info.admin.organization %></dd>
+  
+  <dt><liferay-ui:message key="whois.phone"/></dt>
+  <dd><%= info.admin.phoneNumber %></dd>
+</dl>
+</c:if>
+
+<portlet:actionURL name="whoisLookup" var="whoisUrl"/>
+
+
+<div class="panel panel-default">
+  <div class="panel-body">    
+	<aui:form action="${whoisUrl}" method="post" name="whois">
+		<aui:input name="url" type="text" value="" />
+		<aui:button name="submit" value="Lookup" type="submit"/>
+	</aui:form>
+  </div>
+</div>
